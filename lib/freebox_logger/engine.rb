8cf6e79ed17b6d1f021da1d76c63c8885615e222
Laurent B <lbnetid+gh@gmail.com>
2014-03-02 16:06:18 +0100

    Removed all remaining references to yaml configuration files.

--- lib/freebox_logger/engine.rb
+++ lib/freebox_logger/engine.rb
@@ -1,35 +1,17 @@
-
-class Hash
-  def merge_recursive(o)
-    merge(o) do |_,x,y|
-      if x.respond_to?(:merge_recursive) && y.is_a?(Hash)
-        x.merge_recursive(y)
-      else
-        [*x,*y]
-      end
-    end
-  end
-end
+#-------------------------------------------------------------------------------
+#
+#
+# Copyright (c) 2014 L.Briais under MIT license
+# http://opensource.org/licenses/MIT
+#-------------------------------------------------------------------------------
 
 module FreeboxLogger
   class Engine < ::Rails::Engine
     isolate_namespace FreeboxLogger
 
-
-    def self.merge_config_file filename, conf={}
-      if File.exist? filename
-        Rails.logger.info "Loading #{filename}"
-        file_conf = YAML.load_file(filename)
-        conf = conf.merge_recursive file_conf
-      end
-      conf
-    end
-
-    config.after_initialize  do
-      app_file = File.join Rails.root,'/config/environments/', Rails.env, '/elasticsearch.yml'
-      file = File.exist?(app_file) ? app_file : File.join(self.root,'/config/environments/', Rails.env, '/elasticsearch.yml')
-      config.elastic_servers = YAML.load_file(file)
-    end
+    # Defaut Elasticsearch configuration
+    # Can be overriden in app's environments files.
+    config.elastic_servers = ['127.0.0.1:9200']
 
   end
 end
