85e120dadbe8979ead4cfa587f9538fb7d7e8aee
Laurent B <lbnetid+gh@gmail.com>
2014-10-19 11:48:33 +0200

    Minor fix for tests

--- spec/elastic_record_spec.rb
+++ spec/elastic_record_spec.rb
@@ -13,11 +13,11 @@ describe "When created from real measurements" do
   subject {InternetBoxLogger::FreeboxV5.etl}
 
   it "should not have a #created_at value" do
-    subject.created_at.should be_nil
+    expect( subject.created_at).to be_nil
   end
 
   it "should not have been #saved? automatically" do
-    expect( subject.saved?).to be_false
+    expect( subject.saved?).to be_falsey
   end
 
 end
